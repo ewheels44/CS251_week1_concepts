cb9c8ba98a369f7538899d66091f9a72c4f40a61
eth0 <ethanrobertwheeler@gmail.com>
2021-06-16 11:33:52 -0500

    UPDATE: combined topics of week1 day1

--- week1_concepts.cpp
+++ week1_concepts.cpp
@@ -1,12 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
 struct node {
-  int val;
-  node *next;
+	int val;
+	node *next;
 };
 
 struct List
 {
-   node* Head;
-   int   Count;
+	node *Tail; 	// added tail pointer for convenience
+	node* Head;
+	int   Count;
 };
 
 //
@@ -22,6 +26,12 @@ void lst_free3(node *lst) {
 }
 // deletes nodes right-to-left
 
+//
+// is_sorted
+//
+// NOTE: the data needs to be in accending order???
+// returns true is list is in shorted order
+//
 bool is_sorted(node *lst) {
 
    if(lst == nullptr || lst->next == nullptr)
@@ -32,17 +42,51 @@ bool is_sorted(node *lst) {
 // checks if values are in shorted order
 
 //
+// lst_print
+//
+// prints a linked list
+//
+void lst_print(node *lst){
+
+	if(lst == nullptr)
+		return;
+	else
+	{
+		std::cout << lst->val << " ";
+		lst_print(lst->next);
+	}
+}
+
+//
 // push_front
 //
 // pushes passed in value to fron of linked list
+//
 void push_front(List &mylist, int value1)
 {
 	node *newN = new node;
+	newN->val = value1;
 	newN->next = mylist.Head;
 	mylist.Head = newN;
 
 	mylist.Count++;
+}
 
+void push_back(List &mylist, int value1)
+{
+	node *newN = new node;
+	newN->val = value1;
+	newN->next = nullptr;
+
+	if(mylist.Head == nullptr)
+	{
+		mylist.Head = newN;
+		mylist.Tail = newN;
+	}
+	mylist.Tail->next = newN;
+	mylist.Tail = newN;
+
+	mylist.Count++;
 }
 
 
@@ -54,11 +98,38 @@ int main() {
 
 	for (int i = 0; i < 5; i++)
 	{
-		push_front(mylisyofnodes, i);
+		// push_front
+		//
+		// non-random
+		/* push_front(mylisyofnodes, i); */
+		/* push_front(mylisyofnodes, i + rand() % 10 + 1); */
+
+		// push_back
+		//
+		// non-random
+		push_back(mylisyofnodes, i);
 	}
 
+	// TODO: change list i.e. non-sorted
+	// should also be true cause list is sorted still
+	if(is_sorted(mylisyofnodes.Head))
+	{
+		std::cout << "This list ";
+		lst_print(mylisyofnodes.Head);
+		std::cout << " is sorted" << std::endl;
+	}
+	else
+	{
+		std::cout << "This list ";
+		lst_print(mylisyofnodes.Head);
+		std::cout << " is not sorted" << std::endl;
+	}
+
+
 	lst_free3(mylisyofnodes.Head);
-	is_sorted(mylisyofnodes.Head);
+
+	// should be true cause list should be empty?
+	/* is_sorted(mylisyofnodes.Head); */
 
   return 0;
 }
